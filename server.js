37a476cc35763ece57a14bce18b73b00768f4f18
snakerabbit <alison.s.cheng@gmail.com>
2018-04-16 22:32:56 -0700

    test websocket

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ var bodyParser = require('body-parser');
 
 var app = express();
 var router = express.Router();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
 var port = process.env.API_PORT || 3001;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -21,15 +31,25 @@ router.get('/', function(req, res){
 });
 
 router.get('/streams/publish', function(req, res){
+  //turns on websocket connection to robot and retrieves information
+  //robot sends its ID
+  //server opens websocket connection
+  //robot sends its information periodically
+  //save in local storage
   res.json({message: 'publish route'});
 });
 
 router.get('/streams/subscribe', function(req, res){
+  //subscribes to robot
   res.json({message: 'subscribe route'});
 });
 
-router.get('/robots/metric/:robot_id', function(req, res){
-  res.json({message: `metric route for robot: ${req.params.robot_id}`})
+router.get('/robots/metric/:robot_id/:start-:end', function(req, res){
+  //shows robot distance within a certain time interval
+  res.json({message: `metric route for robot`,
+            robot:req.params.robot_id,
+            start:req.params.start,
+            end: req.params.end})
 });
 
 app.use('/api', router);
